bc0c1876f7e112a0677f7d0e2b43bbedf8b32755
Brant-Harker <brant.harker12@gmail.com>
2024-04-04 15:39:13 -0600

    Masks and med patch
    Functions and code to work with masks easier.
    
    BUT MORE IMPORTANTL, MED PATCH (10) GETS RESULTS BABY

--- main.py
+++ main.py
@@ -4,17 +4,29 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import sys
+import mask
 
-im1 = cv2.imread("CameraPhotos/ATH PATCH (HIST 1).jpg")
-im2 = cv2.imread("CameraPhotos/ATH (12).jpg")
+imurl = "MED PATCH"
+imNum = 10
+
+im1 = cv2.imread(f"CameraPhotos/{imurl} (HIST).jpg")
+mask1 = cv2.imread(f"masks/{imurl} (HIST).jpg")
+if mask1 is None: mask1 = []
+
+im2 = cv2.imread(f"CameraPhotos/{imurl} ({imNum}).jpg")
+mask2 = cv2.imread(f"masks/{imurl} ({imNum}).jpg")
+if mask2 is None: mask2 = []
 
 #BRUTE FORCE
-im1 = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
-im2 = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
+im1 = op.convertGrey(im1)
+im2 = op.convertGrey(im2)
+mask1 = op.convertGrey(mask1)
+mask2 = op.convertGrey(mask2)
+# print(im1.shape, im2.shape, mask1.shape, mask2.shape)
+im2 = cv2.resize(im2, (im1.shape[1], im1.shape[0]), interpolation=cv2.INTER_CUBIC)
+if len(mask2) > 0: mask2 = cv2.resize(mask2, (im1.shape[1], im1.shape[0]), interpolation=cv2.INTER_CUBIC)
 
-#im2 = cv2.resize(im2, (im1.shape[1], im1.shape[0]), interpolation=cv2.INTER_CUBIC)
-
-result,imMatches = pan.stitch(im2,im1, match_type = 1, match_lim=10)
+result,imMatches = pan.stitch(im2,im1, mask2, mask1, match_type = 1, match_lim=10)
 # show the images
 plt.figure(figsize=(15, 10))
 plt.subplot(1,2,1)
