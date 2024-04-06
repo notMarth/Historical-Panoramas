9dd166ea6bcbeca93841db7c3c1b9e3b6537b563
Brant-Harker <brant.harker12@gmail.com>
2024-04-06 14:37:23 -0600

    patch support
    If we end up using a lot of patches

--- Panorama.py
+++ Panorama.py
@@ -10,7 +10,7 @@ def detectAndDescribe(image, feat):
 
     return brisk.detectAndCompute(image, None)
 
-def matchKeypoints(kpsA, kpsB, featuresA, featuresB, match_type, ratio=1):
+def matchKeypoints(kpsA, kpsB, featuresA, featuresB, match_type, ratio=0.7):
 
     bf = cv2.BFMatcher()
     
@@ -35,18 +35,21 @@ def matchKeypoints(kpsA, kpsB, featuresA, featuresB, match_type, ratio=1):
     
     return good_matches, H[0]
 
-def stitch(imageA, imageB, maskA, maskB, match_type = 1, match_lim=-1):
+def stitch(imageA, imageB, maskA, maskB, patchA, patchB, match_type = 1, match_lim=-1, mode=""):
     
     
-    if len(maskA) > 0: kps1, feat1 = detectAndDescribe(maskA, None)
+    if mode == "mask" and len(maskA) > 0: kps1, feat1 = detectAndDescribe(maskA, None)
+    elif mode == "patch" and len(patchA) > 0: kps1, feat1 = detectAndDescribe(patchA, None)
     else: kps1, feat1 = detectAndDescribe(imageA, None)
 
-    if len(maskB) > 0: kps2, feat2 = detectAndDescribe(maskB, None)
+    if mode == "mask" and len(maskB) > 0: kps2, feat2 = detectAndDescribe(maskB, None)
+    elif mode == "patch" and len(maskB) > 0: kps2, feat2 = detectAndDescribe(patchB, None)
     else: kps2, feat2 = detectAndDescribe(imageB, None)
 
     matches, H = matchKeypoints(kps1, kps2, feat1, feat2, match_type=match_type)
     result = np.zeros((2*imageB.shape[0], 2*imageB.shape[1]))
     result += cv2.warpPerspective(imageA, H, (2*imageB.shape[1], 2*imageB.shape[0]))
+    warped = result
     padded = np.pad(imageB, ((0, result.shape[0] - imageB.shape[0]), (0, result.shape[1] - imageB.shape[1])), 'constant', constant_values=0)
     mask = np.ones(result.shape)
     mask[(result>0) * (padded>0)] = 2
@@ -54,4 +57,4 @@ def stitch(imageA, imageB, maskA, maskB, match_type = 1, match_lim=-1):
     
     imMatches = cv2.drawMatches(imageA, kps1, imageB, kps2, matches[:match_lim], None)
     #imMatches=None
-    return (result, imMatches)
+    return (result, imMatches, warped)
--- main.py
+++ main.py
@@ -5,16 +5,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
-imurl = "MED PATCH"
+imurl = "MED"
 imNum = 10
 
 im1 = cv2.imread(f"CameraPhotos/{imurl} (HIST).jpg")
 mask1 = cv2.imread(f"masks/{imurl} (HIST).jpg")
+patch1 = cv2.imread(f"patches/{imurl} (HIST).jpg")
 if mask1 is None: mask1 = []
+if patch1 is None: patch1 = []
 
 im2 = cv2.imread(f"CameraPhotos/{imurl} ({imNum}).jpg")
 mask2 = cv2.imread(f"masks/{imurl} ({imNum}).jpg")
+patch2 = cv2.imread(f"patches/{imurl} ({imNum}).jpg")
 if mask2 is None: mask2 = []
+if patch2 is None: patch2 = []
 
 #BRUTE FORCE
 im1 = op.convertGrey(im1)
@@ -25,7 +29,7 @@ mask2 = op.convertGrey(mask2)
 im2 = cv2.resize(im2, (im1.shape[1], im1.shape[0]), interpolation=cv2.INTER_CUBIC)
 if len(mask2) > 0: mask2 = cv2.resize(mask2, (im1.shape[1], im1.shape[0]), interpolation=cv2.INTER_CUBIC)
 
-result,imMatches = pan.stitch(im2,im1, mask2, mask1, match_type = 1, match_lim=10)
+result,imMatches,warped = pan.stitch(im2,im1, mask2, mask1, patch2, patch1, match_type = 1, match_lim=10, mode="mask")
 # show the images
 plt.figure(figsize=(15, 10))
 plt.subplot(1,2,1)
@@ -44,6 +48,12 @@ plt.imshow(imMatches)
 plt.xticks([]), plt.yticks([])
 plt.title("Matches - Brute Force")
 plt.show()
+
+# plt.figure(figsize=(15, 10))
+# plt.imshow(warped.astype(np.uint8),'gray')
+# plt.xticks([]), plt.yticks([])
+# plt.title("Warp")
+# plt.show()
 
 plt.figure(figsize=(15, 10))
 plt.imshow(result.astype(np.uint8),'gray')
